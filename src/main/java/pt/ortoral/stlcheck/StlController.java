8904fb2997dbf187bd18fe895749640b2dfc091c
Vasco Correia <vascolc@goparity.com>
2026-08-26 15:33:45 +0100

    Add STL file upload functionality to StlController

--- src/main/java/pt/ortoral/stlcheck/StlController.java
+++ src/main/java/pt/ortoral/stlcheck/StlController.java
@@ -1,6 +1,13 @@
 package pt.ortoral.stlcheck;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +18,10 @@ public class StlController {
     @GetMapping("/stl")
     public Stl stl() {
         return new Stl("stl");
+    }
+
+    @PostMapping("readStl")
+    public byte[] readStl(@RequestParam("file") MultipartFile file) throws IOException {
+        return file.getBytes();
     }
 }
